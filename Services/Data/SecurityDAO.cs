5b4227666360b8cb958f7e687b4443b9d0d07135
indrajith76 <indrajithgoswami@gmail.con>
2022-03-21 00:34:17 +0600

    Connect database with login From

--- Services/Data/SecurityDAO.cs
+++ Services/Data/SecurityDAO.cs
@@ -1,6 +1,7 @@
 ﻿using GroceryShop.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,18 +9,44 @@ namespace GroceryShop.Services.Data
 {
     public class SecurityDAO
     {
+        //connect to the database
+        string connectionString = "Data Source=DESKTOP-822836A;Initial Catalog=groceryshop;Integrated Security=True";
+
         internal bool FindByUser(UserModel user)
         {
-            //if (user.Username == "admin" && user.Password == "1234")
-            //{
-            //    return true;
-            //}
-            //else
-            //{
-            //    return false;
-            //}
-            //Alternative way to write the functions.
-            return (user.Username == "admin" && user.Password == "1234");
+            bool success = false;
+
+            //write the sql expression
+            string queryString = "SELECT * FROM dbo.Users Where username = @Username AND password = @Password";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                command.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 50).Value = user.Username;
+                command.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    reader.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return success;g
         }
     }
 }
